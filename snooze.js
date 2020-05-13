25c7418a1483b30c9722b6f9dde60f152deba17c
Nockm <nockmss@gmail.com>
2020-05-13 01:01:09 +0100

    Update snooze.js

--- snooze.js
+++ snooze.js
@@ -1,6 +1,7 @@
-// This creates a promise that pauses
-// the script for the given # of secs
-
+/* 
+	snooze() creates a promise that pauses
+	the script for the given # of secs.
+*/
 function snooze(snoozeTimeSecs) {
   return new Promise((resolveFunc, rejectFunc) => {
     setTimeout(() => {
@@ -9,28 +10,41 @@ function snooze(snoozeTimeSecs) {
   });
 }
 
-// 1.
-// Create a promise to snooze for 3 seconds
+/* 
+	Task 1
+	Use the snooze() function to:
+  
+  - Snooze for 1 second
+  - Print out "I slept for 1 second"
+  
+ 	Note: Don't hardcode the # of seconds
+        in the printout
+*/
 
-// 2.
-// Use the snooze() function to:
-// - Snooze for 1 second
-// - Print out "I slept for 1 second"
-// - Snooze for 2 seconds
-// - Print out "I slept for 2 seconds"
-// - Snooze for 3 seconds
-// - Print out "I slept for 3 seconds"
-//
-// Don't hardcode the # of seconds in the printout
+/*  
+	Task 2.
+	Use the snooze() function to:
+  
+  - Snooze for 1 second
+  - Print out "I slept for 1 second"
+  - Snooze for 2 seconds
+  - Print out "I slept for 2 seconds"
+  - Snooze for 3 seconds
+  - Print out "I slept for 3 seconds"
+*/
 
-// 3.
-// Do the same thing as #2 but use async/await
-// Note: You will have to write it inside
-// the main() method specially crafted.
-// Ignore the stuff around it for now.
+
+/* 
+	Task 3.
+	Do the same thing as #2 but use async/await
+	
+  Note: You will have to write your code inside
+	      the specially-crafted main() method.
+	      Ignore the stuff around it for now.
+*/
 
 async function main() {
-	// Write your stuff here
+	// Write your async/await code here.
 }
 
 (async () => { await main(); })().catch();
