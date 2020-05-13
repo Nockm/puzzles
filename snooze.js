b8e9e54d576327624c1439c3cc591f14c2a09e68
Nockm <nockmss@gmail.com>
2020-05-13 01:09:00 +0100

    Update snooze.js

--- snooze.js
+++ snooze.js
@@ -1,51 +1,57 @@
-/* 
-	snooze() creates a promise that pauses
-	the script for the given # of secs.
+/*
+    Background
+
+    snooze() creates a promise that pauses
+    the script for the given # of secs.
 */
+
 function snooze(snoozeTimeSecs) {
-  return new Promise((resolveFunc, rejectFunc) => {
-    setTimeout(() => {
-      resolveFunc(snoozeTimeSecs);
-    }, snoozeTimeSecs * 1000)
-  });
+    return new Promise((resolveFunc, rejectFunc) => {
+        setTimeout(() => {
+            resolveFunc(snoozeTimeSecs);
+        }, snoozeTimeSecs * 1000)
+    });
 }
 
 /* 
-	Task 1
-	Use the snooze() function to:
-  
-  - Snooze for 1 second
-  - Print out "I slept for 1 second"
-  
- 	Note: Don't hardcode the # of seconds
-        in the printout
+    Task 1
+    
+    Use the snooze() function help you:
+    
+    - Snooze for 1 second
+    - Print out "I slept for 1 second"
+    
+    Note: Don't hardcode the # of seconds in the printout.
 */
 
 /*  
-	Task 2.
-	Use the snooze() function to:
-  
-  - Snooze for 1 second
-  - Print out "I slept for 1 second"
-  - Snooze for 2 seconds
-  - Print out "I slept for 2 seconds"
-  - Snooze for 3 seconds
-  - Print out "I slept for 3 seconds"
+    Task 2
+    
+    Use the snooze() function to help you:
+    
+    - Snooze for 1 second
+    - Print out "I slept for 1 second"
+    - Snooze for 2 seconds
+    - Print out "I slept for 2 seconds"
+    - Snooze for 3 seconds
+    - Print out "I slept for 3 seconds"
+    
+    Note: Use ".then()" Promise chaining.
 */
 
 
 /* 
-	Task 3.
-	Do the same thing as #2 but use async/await
-	
-  Note: You will have to write your code inside
-	      the specially-crafted main() method.
-	      Ignore the stuff around it for now.
+    Task 3
+    
+    Do the same as #2 but use async/await.
+    
+    Note: You will have to write your code inside
+          the specially-crafted main() method.
+          Ignore the stuff around it for now.
 */
 
 async function main() {
-	// Write your async/await code here.
+    // Write your async/await code here.
 }
 
 (async () => { await main(); })().catch();
-
